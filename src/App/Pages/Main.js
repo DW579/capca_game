7a60d715f1cdd5577f88242100b83fe110f8e2a9
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2021-03-30 16:16:47 -0700

    Implemented initial content on Main.js

--- src/App/Pages/Main.js
+++ src/App/Pages/Main.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import { Container, Row, Col, } from "react-bootstrap";
 
 class Main extends Component {
     render() {
         return (
-            <h1>Main</h1>
+            <Container>
+                <Row>
+                    <Col></Col>
+                    <Col><h1>CAPCA Game</h1></Col>
+                    <Col></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h3>Welcome to the CAPCA Game! This game will help you improve your CAPCA skills. Very important for the 21st century workforce :). Please select the amount of rounds you want to place and happy matching!</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         );
     }
 }
